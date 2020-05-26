eefefc99a2a3f212982ab715aa400c2f42394fba
loutn <iamlokep@163.com>
2020-05-27 00:06:30 +0800

    使用css开发

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 借鉴： [ github ](https://github.com/bailicangdu/react-pxq/blob/master/README.md)
 
 1. 通过 ` create-my-app `创建原始项目
+
+
+初次启动项目时要确保已经下载了react的脚手架 create-react-app
+
+否则npm install会失败
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import asyncComponent from '../utils/asyncComponent';
 
+import App from '../App'
+
 // react-router4 不再推荐将所有路由规则放在同一个地方集中式路由，子路由应该由父组件动态配置，组件在哪里匹配就在哪里渲染，更加灵活
 export default class RouteConfig extends Component{
   render(){
     return(
       <HashRouter>
         <Switch>
-          <Route path="/" exact component={asyncComponent(() => import('../app'))} />
-          <Route path="/index" exact component={asyncComponent(() => import('../pages/index'))} />
+          <Route path="/" exact component={App} />
+          <Route path="/index" exact component={asyncComponent(() => import('../pages/index/index'))} />
         </Switch>
       </HashRouter>
     )

44624718c282f5f871ba782869d6486dc2328bf7
lokep <iamlokep@163.com>
2020-06-03 21:02:00 +0800

    u can delete reducers

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import './index.scss'
-
+import propTypes from 'prop-types'
 import Nav from '../../components/nav/index'
 
 class Index extends Component {  
+  //如果没有传递该属性时的默认值
+  static defaultProps = {
+    name: 'stranger'
+  }
+  //如果传递该属性，该属性值必须为字符串
+  static propTypes={
+    name:propTypes.string
+  }
 
   componentWillReceiveProps(nextProps){
     
@@ -12,8 +20,8 @@ class Index extends Component {
   shouldComponentUpdate(nextProps, nextState){
   }
   
-  componentWillMount(){
-
+  componentDidMount(){
+    console.log(this.props)
   }
   render() {
     return (
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import asyncComponent from '../utils/asyncComponent';
-
+import store from '../redux/index'
 import App from '../app'
 import Edit from '../pages/editor/index'
+import { Provider } from 'react-redux';
 const Index =  asyncComponent(() => import('../pages/index/index'))
 
 // react-router4 不再推荐将所有路由规则放在同一个地方集中式路由，子路由应该由父组件动态配置，组件在哪里匹配就在哪里渲染，更加灵活
 export default class RouteConfig extends Component{
   render(){
     return(
-      <HashRouter>
-        <Switch>
-          <Route path="/" exact component={App} />
-          <Route path="/index" exact component={Index} />
-          <Route path="/edit" exact component={Edit} />
-        </Switch>
-      </HashRouter>
+      <Provider store={store}>
+        <HashRouter>
+          <Switch>
+            <Route path="/" exact component={App} />
+            <Route path="/index" exact component={Index} />
+            <Route path="/edit" exact component={Edit} />
+          </Switch>
+        </HashRouter>
+      </Provider>
     )
   }
 }
